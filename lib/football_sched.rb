81d673c5b09a7a48f1392f7f80b43a53bf640387
Paul Beckwith <303dog@gmail.com>
2019-09-09 11:02:43 -0700

    first commit

---
+++ lib/football_sched.rb
@@ -0,0 +1,6 @@
+require "football_sched/version"
+
+module FootballSched
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/football_sched/version.rb
@@ -0,0 +1,3 @@
+module FootballSched
+  VERSION = "0.1.0"
+end
